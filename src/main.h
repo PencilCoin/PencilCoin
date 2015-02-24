cead20990814ea20247304599561110bb5f292dd
PencilCoin <prncilcoin@gmail.com>
2015-02-24 11:38:48 +0700

    Send coin fix

--- src/main.h
+++ src/main.h
@@ -26,7 +26,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const unsigned int MAX_BLOCK_SIZE = 10000;
+static const unsigned int MAX_BLOCK_SIZE = 3000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
